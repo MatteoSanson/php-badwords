78cf97413f3111e04585466fbaf04d0fa2f35329
MatteoSanson <matteo.sanson@gmail.com>
2024-01-08 18:59:50 +0100

    feat: index form

--- index.php
+++ index.php
@@ -7,6 +7,18 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    <h1>ciao</h1>
+    <h1>Form testo da censurare</h1>
+    <p>Inserisci un testo e una parola da censurare, dopodichè premi invio per vedere il risultato.</p>
+    <form action="result.php" method="POST">
+        <label for="testo_utente">Inserisci testo:</label><br>
+        <textarea id="testo_utente" name="testo_utente" rows="4" cols="70"></textarea>
+        
+        <br><br>
+
+        <label for="parola_censurata">Parola da censurare:</label>
+        <input type="text" id="parola_censurata" name="parola_censurata"><br><br>
+
+        <input type="submit" value="Invia">
+    </form>
 </body>
 </html>
